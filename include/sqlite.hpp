3534e0c757113ee7de10cbc57652259529de8523
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2023-09-24 14:56:01 +0500

    Format sqlite.hpp.

--- include/sqlite.hpp
+++ include/sqlite.hpp
@@ -6,42 +6,42 @@
 namespace sqlite {
     class Database {
     public:
-        Database(const std::string& path);
+        Database(const std::string &path);
     };
 
     class Error {
     public:
-        Error(const int code, const std::string& msg);
+        Error(const int code, const std::string &msg);
         const int code() const;
-        const std::string& msg() const;
+        const std::string &msg() const;
         operator bool() const;
     };
 
     class Statement {
     public:
-        Statement(Database& db, const std::string& sql);
+        Statement(Database &db, const std::string &sql);
 
         template <typename T>
-        const Error& bind(const size_t index, const T& value);
+        const Error &bind(const size_t index, const T &value);
     };
 
     class Transaction {
     public:
-        Transaction(Database& db);
-        const Error& commit();
+        Transaction(Database &db);
+        const Error &commit();
     };
 
     class Results {
     private:
         class ResultIterator {
         public:
-            const ResultIterator& operator++();
+            const ResultIterator &operator++();
 
             template <typename T>
-            const T& get(const std::string& column_name) const;
+            const T &get(const std::string &column_name) const;
 
             template <typename T>
-            const T& get(const std::string& column_index) const;
+            const T &get(const std::string &column_index) const;
         };
     public:
         typedef ResultIterator iterator;
@@ -49,17 +49,17 @@ namespace sqlite {
         size_t count() const;
 
         template <typename T>
-        const T& get(const std::string& column_name) const;
+        const T &get(const std::string &column_name) const;
 
         template <typename T>
-        const T& get(const std::string& column_index) const;
+        const T &get(const std::string &column_index) const;
 
-        const iterator& begin() const;
-        const iterator& end() const;
+        const iterator &begin() const;
+        const iterator &end() const;
     };
 
-    const Error exec(Database& db, const std::string& sql, const Results &res = Results());
-    const Error exec(Statement& st, const Results &res = Results());
+    const Error exec(Database &db, const std::string &sql, const Results &res = Results());
+    const Error exec(Statement &st, const Results &res = Results());
 
     const Error OK(0, "");
 };
